ce202f8117989fe96f463f3089bb620ab1933572
Thiago Lima de Oliveira <thiago.loliveira@dataprev.gov.br>
2025-08-12 21:06:36 -0300

    feat: add webview

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import WebView from 'react-native-webview';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <WebView
+        source={{ uri: 'https://google.com/' }}
+        style={styles.webview}
+
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +19,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  webview: {
+    paddingTop: 40,
+    flex: 1,
+  }
 });
